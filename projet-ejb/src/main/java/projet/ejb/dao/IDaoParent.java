02075ccfcc7e3f587a4774838cbccda53565430f
DiehlTalla <talladiehl0@gmail.com>
2024-10-12 14:03:42 +0200

    BD,ENTITE

---
+++ projet-ejb/src/main/java/projet/ejb/dao/IDaoParent.java
@@ -0,0 +1,19 @@
+package projet.ejb.dao;
+
+import java.util.List;
+
+import projet.ejb.data.Parent;
+
+public interface IDaoParent {
+
+	int inserer(Parent parent);
+
+	void modifier(Parent parent);
+
+	void supprimer(int idparent);
+
+	Parent retrouver(int idparent);
+
+	List<Parent> listerTout();
+
+}
